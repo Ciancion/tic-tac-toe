535e78411fc1ee2c938fd2dfeb3660f059f26378
Valentina <valentina.rmo@gmail.com>
2018-02-03 22:43:06 +0000

    Add logic to set winning rows and columns with relative tests

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -9,4 +9,17 @@ describe('Board', function(){
     board.createBoard();
     expect(board.table.length).toEqual(9);
   });
+
+  it('should create the winning combination for the rows', function(){
+    board.createBoard();
+    board.winningCombinationRow();
+    expect(board.winningRows.length).toEqual(3);
+  });
+
+  it('should create the winning combination for the columns', function(){
+    board.createBoard();
+    board.winningCombinationRow();
+    board.winningCombinationColumn();
+    expect(board.winningColumns.length).toEqual(3);
+  });
 });
--- src/board.js
+++ src/board.js
@@ -1,6 +1,10 @@
 function Board(x, y){
   this.numSquares = x * y;
   this.table = [];
+  this.winningRows = [];
+  this.winningColumns = [];
+  this.coordinateX = x;
+  this.coordinateY = y;
 };
 
 Board.prototype.createBoard = function(){
@@ -8,4 +12,22 @@ Board.prototype.createBoard = function(){
     var square = new Square(i);
     this.table.push(square);
   };
+
+  Board.prototype.winningCombinationRow = function( ){
+    for(i = 0; i < this.table.length; i += this.coordinateX){
+      this.winningRows.push(this.table.slice(i, i + this.coordinateX));
+    };
+  };
+
+  Board.prototype.winningCombinationColumn = function(){
+    var sortedSquares = []
+    for(i = 0; i < this.coordinateX; i ++){
+      for(j = 0; j< this.coordinateY; j++){
+        sortedSquares.push(this.winningRows[j][i]);
+      };
+    };
+    for(z = 0; z < sortedSquares.length; z += this.coordinateY){
+      this.winningColumns.push(sortedSquares.slice(z , z + this.coordinateY))
+    };
+  }
 };
